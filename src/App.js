90900d122577b3f8ffdcfaead65e5e72352acff7
Sahil Khadka <sahilkhadka767@gmail.com>
2022-05-14 14:45:42 +0545

    signature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
@@ -11,7 +11,7 @@ const App = () => (
     <AboutUs />
     <SpecialMenu />
     <Chef />
-    <Intro />
+    {/* <Intro /> */}
     <Laurels />
     <Gallery />
     <FindUs />
